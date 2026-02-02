e7c4ff98d15a893375cf3354f51d8191b9a5371c
Mario Bagyinszki <bagyinszki.mario1@gmail.com>
2026-02-02 14:41:02 +0000

    convertingBlueprint

---
+++ Source/FT_Cropout/Public/CPP_CO_MyGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_CO_MyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FT_CROPOUT_API ACPP_CO_MyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite);
+	int _villagerCount = 0;
+	
+};
