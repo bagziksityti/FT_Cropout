54df1bbdedad6a20f425baf4e1a78ff817d014a3
mariogithub <bagyinszki.mario1@gmail.com>
2026-02-08 17:44:32 +0000

    3rd time the charm

---
+++ Source/FT_Cropout/CPP_CO_GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_CO_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FT_CROPOUT_API ACPP_CO_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
